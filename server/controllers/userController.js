39bfc74e80846b262618be8bf2d42db1bd04f8d8
Gulshan Kumar <gulshankumar725044@gmail.com>
2025-07-03 08:21:46 +0530

    Updated Again userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -24,8 +24,8 @@ export const register = async (req, res)=>{
 
         res.cookie('token', token, {
           httpOnly: true,
-          secure: true, // keep false for local dev
-          sameSite: 'None', // change 'strict' to 'lax' for dev
+          secure: true, 
+          sameSite: 'None',
           maxAge: 7 * 24 * 60 * 60 * 1000,
        });
 
@@ -59,8 +59,8 @@ export const login = async (req, res)=>{
 
         res.cookie('token', token, {
            httpOnly: true,
-           secure: false, // keep false for local dev
-           sameSite: 'lax', // change 'strict' to 'lax' for dev
+           secure: true, 
+           sameSite: 'None', 
            maxAge: 7 * 24 * 60 * 60 * 1000,
 });
 
@@ -92,8 +92,8 @@ export const logout = async (req, res)=>{
     try{
         res.clearCookie('token', {
               httpOnly: true,
-              secure: process.env.NODE_ENV === 'production',
-              sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+              secure: true,
+              sameSite: 'None',
 });
         return res.json({success: true, message: "Logged Out"})
     }catch (error){
