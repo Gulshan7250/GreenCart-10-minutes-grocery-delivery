719fb639a5ee8b5374af90476cab12efb2f80d2f
Gulshan Kumar <gulshankumar725044@gmail.com>
2025-07-10 17:50:16 +0530

    AuthSeller is updated

--- server/middlewares/authSeller.js
+++ server/middlewares/authSeller.js
@@ -33,6 +33,6 @@ export const sellerLogout = async (req, res)=>{
         return res.json({success: true, message: "Logged Out"})
     }catch (error){
         console.log(error.message);
-        res.json({success: false, message: error.message});
+        return res.json({success: false, message: error.message});
     }
 }
